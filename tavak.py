fe96811f6a83669ae2ff8b661657d1cda974d3fe
hubinab <hubinab@gmail.com>
2025-01-13 10:35:55 +0100

    3-as feladat átdolgozása

--- tavak.py
+++ tavak.py
@@ -1,25 +1,28 @@
-OsszTerulet = 0
-LegnagyobbToIndex = 0
-LegnagyobbTo = 0
 SzotarLista = []
 
 with open ("alloviz.txt", "r", encoding="utf-8") as filebe:
     sorok = filebe.read().splitlines()
     for i in range(1,len(sorok)):
         adat = sorok[i].strip().split("\t")
-        OsszTerulet += float(adat[2])
-        if int(adat[3]) > LegnagyobbTo:
-            LegnagyobbTo = int(adat[3])
-            LegnagyobbToIndex = i - 1
         SzotarLista.append({"nev":adat[0], "tipus":adat[1], "terulet":adat[2], "vizgyujto":adat[3]})
 
 print("3.a feladat")
 print(f"{i} tó adatait olvastuk be.")
 print("\n")
 
+OsszTerulet = 0
+for i in range(len(SzotarLista)):
+    OsszTerulet += float(SzotarLista[i]["terulet"])
 print("3.b feladat")
 print(f"Magyarország {round((OsszTerulet/93036)*100,2)} %-át fedik le a tavak.")
 print("\n")
+
+LegnagyobbToIndex = 0
+LegnagyobbTo = 0
+for i in range(len(SzotarLista)):
+    if LegnagyobbTo < int(SzotarLista[i]["vizgyujto"]):
+        LegnagyobbTo = int(SzotarLista[i]["vizgyujto"])
+        LegnagyobbToIndex = i
 
 print("3.c feladat")
 print(f"A legnagyobb vízgyűjtő területű állóvíz: {SzotarLista[LegnagyobbToIndex]['nev']}")
